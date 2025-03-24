2cc7e9ee04b3684e88970a45e5e49665471fbdf9
Rakib7512 <mdrakibislam7512@gmail.com>
2025-03-24 14:49:35 +0600

    24-03-25
    24-03-25

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,19 @@
+package javaapplication2;
+
+
+public class JavaApplication2 {
+
+    
+    public static void main(String[] args) {
+        String name="Rakib";
+        byte id=123;
+        char latter ='\u0041';
+        short number= 25441;
+        int numbr2=12235255;
+        float number3=6525274852282.1252558585225f;
+    System.out.println("Hello "+name);
+    System.out.println(latter);
+    }
+    
+}
+
