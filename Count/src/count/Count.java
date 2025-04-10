ade37a47547c3050572a934269da1f88c065506f
Rakib7512 <mdrakibislam7512@gmail.com>
2025-04-10 19:05:46 +0600

    10
    10

--- Count/src/count/Count.java
+++ Count/src/count/Count.java
@@ -11,13 +11,16 @@ public class Count {
         Scanner input=new Scanner(System.in);
         System.out.println("Enter Input");
         int usurinput=input.nextInt();
+        
+        System.out.println("Enter Text");
+        String name=input.next();
                 
           int count=0;
 
         for (int i = 1; i <= usurinput; i++) {
 
            count++;            
-            System.out.println("love");
+            System.out.println(name);
         }
     
         
