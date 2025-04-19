364e146a7aa5a829a0607ea5beeb4c5860d64a76
Rakib7512 <mdrakibislam7512@gmail.com>
2025-04-20 01:27:09 +0600

    constracktor
    constracktor

--- Constract2/src/main/java/com/mycompany/constract2/Constract2.java
+++ Constract2/src/main/java/com/mycompany/constract2/Constract2.java
@@ -2,9 +2,17 @@
 
 package com.mycompany.constract2;
 
+import com.mycompany.constract2.Squire.Squire;
+
 public class Constract2 {
 
     public static void main(String[] args) {
+        
+        Squire s= new Squire(5);
+        
+        System.out.println("Squire Area is "+s.getSquireArea());
+        
+        
 
     }
 }
--- Constract2/src/main/java/com/mycompany/constract2/Squire/Squire.java
+++ Constract2/src/main/java/com/mycompany/constract2/Squire/Squire.java
@@ -4,9 +4,32 @@ package com.mycompany.constract2.Squire;
 
 public class Squire {
 
+    double lenth;
+    double with;
+    
+    
     public Squire() {
     }
+
+    public Squire(double lenth, double dith) {
+        this.lenth = lenth;
+        this.with = dith;
+    }
+
+    public Squire(double lenth) {
+        this.lenth = lenth;
+    }
     
+    
+    
+    public double getSquireArea(){
+    
+    double result=Math.pow(lenth, 2);
+    
+    return result;
+    
+    
+    }
     
     
     
