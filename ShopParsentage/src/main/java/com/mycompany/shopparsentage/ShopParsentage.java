fd9f3f0dac4a636f3131ed0e5d969d253a3c780f
Rakib7512 <mdrakibislam7512@gmail.com>
2025-05-03 13:48:08 +0600

    03-05-25
    03-05-25

---
+++ ShopParsentage/src/main/java/com/mycompany/shopparsentage/ShopParsentage.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.shopparsentage;
+
+/**
+ *
+ * @author RJ Power
+ */
+public class ShopParsentage {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
