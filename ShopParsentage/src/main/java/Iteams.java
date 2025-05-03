49627556133c4694ae9a7dca9f161835e02e2ade
Rakib7512 <mdrakibislam7512@gmail.com>
2025-05-03 14:01:36 +0600

    65
    69

--- ShopParsentage/src/main/java/Iteams.java
+++ ShopParsentage/src/main/java/Iteams.java
@@ -1,12 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author RJ Power
- */
 public class Iteams {
+    
+    double quentaty;
+    String productName;
+    
+    double price;
+    String quality;
+
+    public Iteams() {
+    }
+
+    public Iteams(double quentaty, String productName, double price, String quality) {
+        this.quentaty = quentaty;
+        this.productName = productName;
+        this.price = price;
+        this.quality = quality;
+    }
+
+    public double getQuentaty() {
+        return quentaty;
+    }
+
+    public void setQuentaty(double quentaty) {
+        this.quentaty = quentaty;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
     
 }
