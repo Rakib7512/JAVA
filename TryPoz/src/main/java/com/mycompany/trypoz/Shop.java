6e1ca2caa2a58e5c436d1cd038223f270cc89a11
Rakib7512 <mdrakibislam7512@gmail.com>
2025-05-07 02:59:01 +0600

    poz
    poz

--- TryPoz/src/main/java/com/mycompany/trypoz/Shop.java
+++ TryPoz/src/main/java/com/mycompany/trypoz/Shop.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.trypoz;
 
-/**
- *
- * @author RJ Power
- */
 public class Shop {
-    
-}
+    double price;
+     String productName;
+     double quentaty;
+
+    public Shop() {
+    }
+
+    public Shop(double price, String productName, double quentaty) {
+        this.price = price;
+        this.productName = productName;
+        this.quentaty = quentaty;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getQuentaty() {
+        return quentaty;
+    }
+
+    public void setQuentaty(double quentaty) {
+        this.quentaty = quentaty;
+    }
+public void getPriceQuantaty(){
+    System.out.println(productName+price+quentaty);
+};
+
+
+    }
+     
+     
+
