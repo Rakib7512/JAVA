a1eeeea2a99aa6f1f6dbb2afe663ea99a1f9ed12
Md Rakib Islam <mdrakibislam7512@gmail.com>
2025-05-10 19:11:34 +0600

    nj
    bh

---
+++ EmployeesOOP/src/main/java/com/mycompany/employeesoop/EmployeesOOP.java
@@ -0,0 +1,13 @@
+
+
+package com.mycompany.employeesoop;
+
+
+public class EmployeesOOP {
+
+    public static void main(String[] args) {
+Manager m=new Manager(12, "Rakib", 1240, 1200);
+
+    }
+    
+}
---
+++ EmployeesOOP/src/main/java/com/mycompany/employeesoop/Manager.java
@@ -0,0 +1,39 @@
+
+package com.mycompany.employeesoop;
+
+import com.mycompany.employeesoop.e.Employee;
+
+
+public class Manager extends Employee{
+    private int teamSize;
+
+    public Manager() {
+    }
+
+    public Manager(int teamSize) {
+        this.teamSize = teamSize;
+    }
+
+    public Manager(int teamSize, String name, int id, int salary) {
+        super(name, id, salary);
+        this.teamSize = teamSize;
+    }
+
+    public int getTeamSize() {
+        return teamSize;
+    }
+
+    public void setTeamSize(int teamSize) {
+        this.teamSize = teamSize;
+    }
+
+    @Override
+    public String toString() {
+      
+       return teamSize+super.toString(); 
+              
+     
+   }
+    
+    
+}
