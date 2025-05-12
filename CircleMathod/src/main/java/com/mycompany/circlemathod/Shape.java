36be3db183bd9462c1fed870d2f2e06269b216d6
Md Rakib Islam <mdrakibislam7512@gmail.com>
2025-05-12 19:02:49 +0600

    ]
    ]]

--- CircleMathod/src/main/java/com/mycompany/circlemathod/Shape.java
+++ CircleMathod/src/main/java/com/mycompany/circlemathod/Shape.java
@@ -2,21 +2,27 @@
 package com.mycompany.circlemathod;
 
 public class Shape {
-    private double area;
-
-    public Shape() {
-    }
-
-    public Shape(double area) {
-        this.area = area;
-    }
-
-    public double getArea() {
-        return area;
-    }
-
-    public void setArea(double area) {
-        this.area = area;
-    }
+//    private double area;
+//
+//    public Shape() {
+//    }
+//
+//    public Shape(double area) {
+//        this.area = area;
+//    }
+//
+//    public double getArea() {
+//        return area;
+//    }
+//
+//    public void setArea(double area) {
+//        this.area = area;
+//    }
   
+    
+    public double area(){
+    
+    return 0.0;
+    }
+    
 }
