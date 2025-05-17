b54850fe4714fb927551658d92bfe606c6dee2f5
Md Rakib Islam <mdrakibislam7512@gmail.com>
2025-05-17 18:46:59 +0600

    dbc
    dbc

---
+++ LearnDatabaseConnection/src/learndatabaseconnection/view/DbUtil.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package learndatabaseconnection.view;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Admin
+ */
+public class DbUtil {
+    
+    
+    private Connection con=null;
+    private String url="jdbc://";
+    private String user="";
+    private String password="";
+    private String Driver="";
+     
+}
