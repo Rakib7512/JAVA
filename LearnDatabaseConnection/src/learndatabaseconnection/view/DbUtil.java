0e932bfbdbfc2fc35fcf325c773dffeda0fff2f6
Md Rakib Islam <mdrakibislam7512@gmail.com>
2025-05-17 18:55:38 +0600

    dbc
    dbc

--- LearnDatabaseConnection/src/learndatabaseconnection/view/DbUtil.java
+++ LearnDatabaseConnection/src/learndatabaseconnection/view/DbUtil.java
@@ -1,22 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package learndatabaseconnection.view;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author Admin
- */
 public class DbUtil {
     
     
     private Connection con=null;
-    private String url="jdbc://";
-    private String user="";
-    private String password="";
-    private String Driver="";
+    private final String url="jdbc:mysql://localhost:3306/mysql";
+    private final String user="root";
+    private final String password="1234";
+    private final String Driver="com.mysql.cj.jdbc.Driver";
      
+    public Connection getCon(){
+        
+        try {
+            Class.forName(Driver);
+                con = DriverManager.getConnection(url, user, password);
+            }
+        catch (SQLException | ClassNotFoundException ex) {
+                Logger.getLogger(DbUtil.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+        return con;
+    }
 }
