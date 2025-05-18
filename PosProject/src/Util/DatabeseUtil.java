238abea2b379752ac5b378d0b51f8b0ea24a25b6
Rakib7512 <mdrakibislam7512@gmail.com>
2025-05-19 00:21:58 +0600

    pos project
    pos project

---
+++ PosProject/src/Util/DatabeseUtil.java
@@ -0,0 +1,30 @@
+
+package Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DatabeseUtil {
+    private Connection con=null;
+    private String url="jdbc:mysql://localhost:3306/pos";
+    private String usre="root";
+    private String password="1234";
+    private String driver="com.mysql.cj.jdbc.Driver";
+    
+  public Connection getCon(){
+        try {
+            Class.forName(driver);
+            con=DriverManager.getConnection(url, usre, password);
+            
+        } catch (ClassNotFoundException |SQLException ex) {
+            Logger.getLogger(DatabeseUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    return con;
+    
+    }
+}
