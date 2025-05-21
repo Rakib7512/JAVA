dfe55c9cdf624133f7ecf8fa0a4f662517bd0e4d
Rakib7512 <mdrakibislam7512@gmail.com>
2025-05-21 13:25:00 +0600

    CSDU
    CSDU

--- CRUD/src/crud/CRUD.java
+++ CRUD/src/crud/CRUD.java
@@ -2,6 +2,7 @@
 package crud;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,11 +17,20 @@ public class CRUD {
 
     public static void main(String[] args) {
         
-  
+        System.out.println("------After Save----");
+        saveEmp("Rakib", "rakib@gmail.com", 50000);
+        showAllEmp();
         
+        
+        System.out.println("-------After Edit-----");
+        updateEmp("Rakib Islam", "rakib@gmail.com", 70000, 1);
+        showAllEmp();
+        System.out.println("-------After Delete");
+        deleteEmp(1);
+        showAllEmp();
   
     }
-    public void saveEmp(String name,String email,float salary){
+    public static void saveEmp(String name,String email,float salary){
     
     sql="insert into emp(name,email,salary) values(?,?,?)";
         try {
@@ -37,6 +47,69 @@ public class CRUD {
         } catch (SQLException ex) {
             Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, ex);
         }
+    
+    }
+     public static void showAllEmp(){
+     sql="select from emp";
+        try {
+            ps=du.getCon().prepareStatement(sql);
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+            
+            int id=rs.getInt("id");
+            String name=rs.getString("name");
+            String email=rs.getString("email");
+            float salary=rs.getFloat("salary");
+                System.out.println("ID: "+id+"Name: "+name+"Email: "+email+"Salary: "+salary);
+            
+            }
+            rs.close();
+            ps.cancel();
+            du.getCon().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+     
+     }
+    public static void deleteEmp(int id){
+    sql="delete from emp where id=?";
+        try {
+            ps=du.getCon().prepareStatement(sql);
+            ps.setInt(1, id); 
+            
+            
+            ps.executeUpdate();
+            ps.cancel();
+            du.getCon().close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+   
+    
+    }
+    
+    public static void updateEmp(String name,String email,float salary,int id){
+    sql="update emp set name=?, email=?,salary=? where id=?";
+        try {
+            ps=du.getCon().prepareStatement(sql);
+           ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setFloat(3, salary);
+            ps.setInt(4, id);
+            
+            
+            ps.executeUpdate();
+            ps.close();
+            du.getCon().close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    
+    
     
     }
 }
