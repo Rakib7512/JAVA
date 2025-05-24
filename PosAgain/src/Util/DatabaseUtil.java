1667fdad9296bd22d98e524509ee3fb4d99a374b
Md Rakib Islam <mdrakibislam7512@gmail.com>
2025-05-24 19:08:28 +0600

    pos
    pos

---
+++ PosAgain/src/Util/DatabaseUtil.java
@@ -0,0 +1,27 @@
+
+package Util;
+
+import com.sun.jdi.connect.spi.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DatabaseUtil {
+     private Connection con=null;
+    private String url="jdbc:mysql://localhost:3306/posAgain";
+    private String usre="root";
+    private String password="1234";
+    private String driver="com.mysql.cj.jdbc.Driver";
+    
+    public Connection getCon(){
+    
+         try {
+             Class.forName(driver);
+             con=(Connection) DriverManager.getConnection(url, usre, password);
+         } catch (ClassNotFoundException |SQLException ex) {
+             Logger.getLogger(DatabaseUtil.class.getName()).log(Level.SEVERE, null, ex);
+         }
+  return con;
+    }
+}
---
+++ PosAgain/src/posagain/PosAgain.java
@@ -0,0 +1,11 @@
+
+package posagain;
+
+public class PosAgain {
+
+
+    public static void main(String[] args) {
+ 
+    }
+    
+}
