9abca64c2d1f3d95d4969c0f4b1fe8df88dd7f30
Rakib7512 <mdrakibislam7512@gmail.com>
2025-06-02 14:17:37 +0600

    Update ProductDao.java

--- PosAgain/src/Dao/ProductDao.java
+++ PosAgain/src/Dao/ProductDao.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Dao;
 
-/**
- *
- * @author RJ Power
- */
+import Util.DatabaseUtil;
+import entity.Category;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+import javax.swing.JComboBox;
+
+
 public class ProductDao {
+    DatabaseUtil du=new DatabaseUtil();
+    PreparedStatement ps;
+    String sql;
+    ResultSet rs;
+  CategoryDao categoryDao=new CategoryDao();
+    public void loadCategoryToProductCombo(JComboBox<String>catList){
+  catList.removeAllItems();
+        List<Category> categories=new java.util.ArrayList<>();
+        categories=categoryDao.getAllCategory();
+        
+    
+    
+    
+    }
     
 }
